2cc40473e9ef43fe70a4b99003258b9c95c7df1f
inan_hypehype <137870130+inanhh@users.noreply.github.com>
2024-11-20 11:42:29 +0200

    Update HhScreenRecorderPlugin.swift
    test

--- macos/Classes/HhScreenRecorderPlugin.swift
+++ macos/Classes/HhScreenRecorderPlugin.swift
@@ -24,7 +24,7 @@ public class HhScreenRecorderPlugin: NSObject, FlutterPlugin,
           
           if (call.method == "startRecording")
           {
-	      result.error("test", "test2", null);
+	      result.error("test", "test2", nil);
               var enableMicrophone = false
               if let arguments = call.arguments as? [String: Any] {
                  enableMicrophone = arguments["enableMicrophone"] as! Bool
